00c8dce98d8fad900f3635ed3cfd49ec9c070852
sguiscardo <sdguiscardo@gmail.com>
2023-02-09 13:09:40 -0600

    UpdatedViewControllers

---
+++ TravelDiary/View Controllers/Custom Views/EntryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  EntryTableViewCell.swift
+//  TravelDiary
+//
+//  Created by Sebastian Guiscardo on 2/9/23.
+//
+
+import UIKit
+
+class EntryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
